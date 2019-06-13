b746ee97ff042cbb2021f699b94aadc42027553d
XPS-9560\Dorian <popa.dorian98@gmail.com>
2019-06-13 03:57:33 +0300

    Sessions screen refactoring

--- GameGang/application/view/profile/sessions.php
+++ GameGang/application/view/profile/sessions.php
@@ -1,38 +1,37 @@
 <div class="container">
-  <?php if($this->isLoggedIn()) { ?>
-    <div class="box">
-        <h3>Add a session</h3>
-        <form action="<?php echo URL; ?>profile/addsession" method="POST">
-            <label>Game</label>
-            <input type="text" name="title" value="" required />
-            <label>Description</label>
-            <input type="text" name="description" value="" required />
-            <label>Duration</label>
-            <input type="text" name="duration" value="" placeholder="hh:mm:ss.xxxx" />
-            <input type="hidden" name="user_id" value="<?php echo $this->isLoggedIn(); ?>">
-            <input type="submit" name="submit_add_session" value="Submit" />
-        </form>
-    </div>
-  <?php } ?>
-  <div class="box">
-    <table>
-        <thead style="background-color: #ddd; font-weight: bold;">
-        <tr>
-            <td>Title</td>
-            <td>Description</td>
-            <td>Duration</td>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($sessions as $session) { ?>
+  <div class="sessionContainer">
+    <?php if($this->isLoggedIn()) { ?>
+      <div class="addSessionField">
+          <h3>Add a new game session</h3>
+          <form action="<?php echo URL; ?>profile/addsession" method="POST">
+              <input type="text"    class="inputField" name="title" placeholder="Game" value="" required />
+              <input type="text"    class="inputField" name="description" placeholder="Description" value="" required />
+              <input type="text"    class="inputField" name="duration" value="" placeholder="hh:mm:ss.xxxx" />
+              <input type="hidden" name="user_id" value="<?php echo $this->isLoggedIn(); ?>">
+              <input type="submit"  class="submitButton" name="submit_add_session" value="Submit" />
+          </form>
+      </div>
+    <?php } ?>
+    <div class="mySessionsTable">
+      <h3>My game sessions</h3>
+      <table>
+          <thead>
             <tr>
-                <td><?php if (isset($session->title)) echo htmlspecialchars($session->title, ENT_QUOTES, 'UTF-8'); ?></td>
-                <td><?php if (isset($session->description)) echo htmlspecialchars($session->description, ENT_QUOTES, 'UTF-8'); ?></td>
-                <td><?php if (isset($session->duration)) echo htmlspecialchars($session->duration, ENT_QUOTES, 'UTF-8'); ?></td>
+                <td>Title</td>
+                <td>Description</td>
+                <td>Duration</td>
             </tr>
-        <?php } ?>
-        </tbody>
-    </table>
-
+          </thead>
+          <tbody>
+          <?php foreach ($sessions as $session) { ?>
+              <tr>
+                  <td><?php if (isset($session->title)) echo htmlspecialchars($session->title, ENT_QUOTES, 'UTF-8'); ?></td>
+                  <td><?php if (isset($session->description)) echo htmlspecialchars($session->description, ENT_QUOTES, 'UTF-8'); ?></td>
+                  <td><?php if (isset($session->duration)) echo htmlspecialchars($session->duration, ENT_QUOTES, 'UTF-8'); ?></td>
+              </tr>
+          <?php } ?>
+          </tbody>
+      </table>
+    </div>
   </div>
 </div>
